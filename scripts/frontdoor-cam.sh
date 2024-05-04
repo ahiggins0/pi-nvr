07da9aae6af86447d6d5a1f76a185cc653ec5b92
ahiggins0 <37230314+ahiggins0@users.noreply.github.com>
2024-05-04 09:10:26 -0500

    Create frontdoor-cam.sh

---
+++ scripts/frontdoor-cam.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /mnt/usb/frontdoor
+openRTSP -D 1 -c -B 10000000 -b 10000000 -4 -f 15 -Q -F frontdoor-cam -P 3600 -t -K -u <username> <password> "rtsp://<rpi-ip-address>:554/cam/realmonitor?channel=1&subtype=0&authbasic=64"
